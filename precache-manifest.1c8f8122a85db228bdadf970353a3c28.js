4fecd19e38a1d4d851fa98ab963dbf630d5f8209
Dev51 <33731541+Deveveloper@users.noreply.github.com>
2020-03-29 03:29:29 +0300

    vk

---
+++ precache-manifest.1c8f8122a85db228bdadf970353a3c28.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b56024dd68470e5fdade",
+    "url": "/static/css/main.8a7ef0a3.chunk.css"
+  },
+  {
+    "revision": "b56024dd68470e5fdade",
+    "url": "/static/js/main.b56024dd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "3b0dbe3502dd694a13e2",
+    "url": "/static/css/2.b9dd2560.chunk.css"
+  },
+  {
+    "revision": "3b0dbe3502dd694a13e2",
+    "url": "/static/js/2.3b0dbe35.chunk.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/static/media/persik.4e1ec840.png"
+  },
+  {
+    "revision": "26f22bbd3d52af9dcaba32cca6ea7776",
+    "url": "/index.html"
+  }
+];
